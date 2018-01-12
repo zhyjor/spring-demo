cc89fd3532ca4db924ef6cc8312c94a716cb7c45
zhyjor <zhyjor@163.com>
2018-01-13 00:16:56 +0800

    test

--- src/main/java/com/zhyj/demo/controller/HelloWorldController.java
+++ src/main/java/com/zhyj/demo/controller/HelloWorldController.java
@@ -1,12 +1,16 @@
 package com.zhyj.demo.controller;
 
+import com.zhyj.demo.domain.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping("/hello")
-    public String index() {
-        return "Hello World";
+    @RequestMapping("/getUser")
+    public User getUser() {
+        User user = new User();
+        user.setUserName("小明");
+        user.setPassWord("xxxx");
+        return user;
     }
 }
---
+++ src/main/java/com/zhyj/demo/domain/User.java
@@ -0,0 +1,22 @@
+package com.zhyj.demo.domain;
+
+public class User {
+    private String userName;
+    private String passWord;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
